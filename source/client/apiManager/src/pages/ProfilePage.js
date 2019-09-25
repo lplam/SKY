e828bd34ce081fd2c124123c65d0b0d05891580b
nguyenbaky <43399274+nguyenbaky@users.noreply.github.com>
2019-09-25 23:25:17 +0700

    Add files via upload

---
+++ source/client/apiManager/src/pages/ProfilePage.js
@@ -0,0 +1,264 @@
+import React, {Component} from "react";
+import MenuPage from './../../components/Menu/Menu'
+
+
+class ProfilePage extends Component{
+    render(){
+        return(
+        <div>
+              <link rel="stylesheet" href="./servicesStyle/css/style.css"/>
+                <div id="top-header" className="hello">
+                    <div id = "contact-header">
+                        <span className = "green-color mr-r-50"><i className="fa fa-question-circle-o green-color icon-mr-r-10" aria-hidden="true"></i>Have a question?</span>
+                        <span className = "green-color mr-r-50"><i className="fa fa-phone green-color icon-mr-r-10" aria-hidden="true"></i>0344 656 534</span>
+                        <span className = "green-color mr-r-50"><i className="fa fa-envelope-o green-color icon-mr-r-10" aria-hidden="true"></i>sound@myapi.com</span>
+                    </div>
+
+                <div class="ml-auto">
+                    <div class="social-wrap">
+                    <a href="#"><span class="icon-facebook"><i class="fa fa-facebook" aria-hidden="true"></i></span></a>
+                    <a href="#"><span class="icon-twitter"><i class="fa fa-twitter" aria-hidden="true"></i></span></a>
+                    <a href="#"><span class="icon-linkedin"><i class="fa fa-instagram" aria-hidden="true"></i></span></a>
+
+                    <a href="#" class="d-inline-block d-lg-none site-menu-toggle js-menu-toggle text-black"><span
+                        class="icon-menu h3"></span></a>
+                   </div>
+               </div>                
+
+                </div>   
+
+                <MenuPage match = {this.props.match}/>
+
+                <div>
+        
+        <link href="./servicesStyle/css/bootstrap.min.css" rel="stylesheet" />  
+
+        <hr />
+        <div className="container bootstrap snippet">
+          <div className="row">
+            <div className="col-sm-10"><h1>User name</h1></div>
+            {/* <div className="col-sm-2"><a href="/users" className="pull-right"><img title="profile image" className="img-circle img-responsive" src="http://www.gravatar.com/avatar/28fd20ccec6865e2d5f0e1f4446eb7bf?s=100" /></a></div> */}
+          </div>
+          <div className="row">
+            <div className="col-sm-3">{/*left col*/}
+              <div className="text-center">
+                <img src="./servicesStyle/images/person_1.jpg" className="avatar img-circle img-thumbnail" alt="avatar" />
+                <h6>Upload a different photo...</h6>
+                <input type="file" className="text-center center-block file-upload" />
+              </div><br />
+              <div className="panel panel-default">
+                <div className="panel-heading">Website <i className="fa fa-link fa-1x" /></div>
+                <div className="panel-body"><a href="http://bootnipets.com">bootnipets.com</a></div>
+              </div>
+             
+              <div className="panel panel-default">
+                <div className="panel-heading">Social Media</div>
+                <div className="panel-body">
+                  <i className="fa fa-facebook fa-2x" /> <i className="fa fa-github fa-2x" /> <i className="fa fa-twitter fa-2x" /> <i className="fa fa-pinterest fa-2x" /> <i className="fa fa-google-plus fa-2x" />
+                </div>
+              </div>
+            </div>{/*/col-3*/}
+            <div className="col-sm-9">
+              <ul className="nav nav-tabs">
+                <li className="active"><a data-toggle="tab" href="#home">Home</a></li>
+                <li><a data-toggle="tab" href="#messages">Menu 1</a></li>
+                <li><a data-toggle="tab" href="#settings">Menu 2</a></li>
+              </ul>
+              <div className="tab-content">
+                <div className="tab-pane active" id="home">
+                  <hr />
+                  <form className="form" action="##" method="post" id="registrationForm">
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="first_name" style={{margin:"5px"}}><h4>First name</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="first_name" id="first_name" placeholder="first name" title="enter your first name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="last_name" style={{margin:"5px"}}><h4>Last name</h4></label>
+                        <input type="text" className="form-control"  style={{height:"34px" ,margin:"5px"}} name="last_name" id="last_name" placeholder="last name" title="enter your last name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="phone" style={{margin:"5px"}}><h4>Phone</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="mobile" style={{margin:"5px"}}><h4>Card Number</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="mobile" id="mobile" placeholder="enter card number" title="enter your mobile number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Username</h4></label>
+                        <input type="email" className="form-control" style={{height:"34px" ,margin:"5px"}} name="email" id="email" placeholder="you@email.com" title="enter your email." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Location</h4></label>
+                        <input type="email" className="form-control" style={{height:"34px" ,margin:"5px"}} id="location" placeholder="somewhere" title="enter a location" />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password" style={{margin:"5px"}}><h4>Password</h4></label>
+                        <input type="password" className="form-control"  style={{height:"34px" ,margin:"5px"}} name="password" id="password" placeholder="password" title="enter your password." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password2" style={{margin:"5px"}}><h4>Verify</h4></label>
+                        <input type="password" className="form-control" style={{height:"34px" ,margin:"5px"}} name="password2" id="password2" placeholder="password2" title="enter your password2." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-12">
+                        <br />
+                        <button className="btn btn-lg btn-success" type="submit"><i className="glyphicon glyphicon-ok-sign" /> Save</button>
+                        <button className="btn btn-lg" type="reset"><i className="glyphicon glyphicon-repeat" /> Reset</button>
+                      </div>
+                    </div>
+                  </form>
+                  <hr />
+                </div>{/*/tab-pane*/}
+                <div className="tab-pane" id="messages">
+                  <h2 />
+                  <hr />
+                  <form className="form" action="##" method="post" id="registrationForm">
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="first_name" style={{margin:"5px"}}><h4>First name</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="first_name" id="first_name" placeholder="first name" title="enter your first name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="last_name" style={{margin:"5px"}}><h4>Last name</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="last_name" id="last_name" placeholder="last name" title="enter your last name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="phone" style={{margin:"5px"}}><h4>Phone</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="mobile" style={{margin:"5px"}}><h4>Mobile</h4></label>
+                        <input type="text" className="form-control" style={{height:"34px" ,margin:"5px"}} name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Email</h4></label>
+                        <input type="email" className="form-control" style={{height:"34px" ,margin:"5px"}} name="email" id="email" placeholder="you@email.com" title="enter your email." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Location</h4></label>
+                        <input type="email" className="form-control" style={{height:"34px" ,margin:"5px"}} id="location" placeholder="somewhere" title="enter a location" />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password" style={{margin:"5px"}}><h4>Password</h4></label>
+                        <input type="password" className="form-control" style={{height:"34px" ,margin:"5px"}} name="password" id="password" placeholder="password" title="enter your password." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password2" style={{margin:"5px"}}><h4>Verify</h4></label>
+                        <input type="password" className="form-control" style={{height:"34px" ,margin:"5px"}} name="password2" id="password2" placeholder="password2" title="enter your password2." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-12">
+                        <br />
+                        <button className="btn btn-lg btn-success" type="submit"><i className="glyphicon glyphicon-ok-sign" /> Save</button>
+                        <button className="btn btn-lg" type="reset"><i className="glyphicon glyphicon-repeat" /> Reset</button>
+                      </div>
+                    </div>
+                  </form>
+                </div>{/*/tab-pane*/}
+                <div className="tab-pane" id="settings">
+                  <hr />
+                  <form className="form" action="##" method="post" id="registrationForm">
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="first_name" style={{margin:"5px"}}><h4>First name</h4></label>
+                        <input type="text" className="form-control" name="first_name" id="first_name" placeholder="first name" title="enter your first name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="last_name" style={{margin:"5px"}}><h4>Last name</h4></label>
+                        <input type="text" className="form-control" name="last_name" id="last_name" placeholder="last name" title="enter your last name if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="phone" style={{margin:"5px"}}><h4>Phone</h4></label>
+                        <input type="text" className="form-control" name="phone" id="phone" placeholder="enter phone" title="enter your phone number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="mobile" style={{margin:"5px"}}><h4>Mobile</h4></label>
+                        <input type="text" className="form-control" name="mobile" id="mobile" placeholder="enter mobile number" title="enter your mobile number if any." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Email</h4></label>
+                        <input type="email" className="form-control" name="email" id="email" placeholder="you@email.com" title="enter your email." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="email" style={{margin:"5px"}}><h4>Location</h4></label>
+                        <input type="email" className="form-control" id="location" placeholder="somewhere" title="enter a location" />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password" style={{margin:"5px"}}><h4>Password</h4></label>
+                        <input type="password" className="form-control" name="password" id="password" placeholder="password" title="enter your password." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-6">
+                        <label htmlFor="password2" style={{margin:"5px"}}><h4>Verify</h4></label>
+                        <input type="password" className="form-control" name="password2" id="password2" placeholder="password2" title="enter your password2." />
+                      </div>
+                    </div>
+                    <div className="form-group">
+                      <div className="col-xs-12">
+                        <br />
+                        <button className="btn btn-lg btn-success pull-right" type="submit"><i className="glyphicon glyphicon-ok-sign" /> Save</button>
+                        {/*<button class="btn btn-lg" type="reset"><i class="glyphicon glyphicon-repeat"></i> Reset</button>*/}
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>{/*/tab-pane*/}
+            </div>{/*/tab-content*/}
+          </div>{/*/col-9*/}
+        </div>{/*/row*/}
+
+      </div>
+      
+            
+                </div>
+        )
+    }
+        
+}
+
+export default ProfilePage;
