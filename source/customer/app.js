8686099fa789578e7b9c8d827b4a7ff346637223
lephuclam97 <lamboyboy20@gmail.com>
2019-10-11 18:09:50 +0700

    edit api server

--- source/customer/app.js
+++ source/customer/app.js
@@ -4,18 +4,38 @@ const bodyParser = require('body-parser');
 const request = require('request')
 const PORT = 8080;
 
-let apiCall = require('./api.js')
 // SDK example
-let key = "5c9676a9f448181b63448971"
+let key = "5c9676a997f01eb3d30e05167"
 let word = "hello"
-let url = 'http://localhost:4000/engvies/'
-//apiCall.callApi(url,key,word)
-
-// apiCall.callApi("1",key,word)
+let urls = 'http://localhost:4000/engvies/'
 
 app.get('/', (req,res)=>{
-    res.send(apiCall.callApi(url,key,word)+ "hello")
+
+    request({
+        url: urls + key +"/true/" + word,
+        method: 'GET',
+        timeout: 10000,
+    },function(err,data,body) {
+        
+        console.log(data.body)
+        res.json(data.body)
+
+    })
+
 })
 
+//SDK NODEJS
+
+
+
+
+
+
+
+
+
+
+
+
 app.use(bodyParser.json());
-app.listen(PORT,()=> console.log('Listening on PORT '+ PORT));
+app.listen(PORT,()=> console.log('Listening on PORT '+ PORT))
